32737669357d11cad18eb026f5a78b3f3f778c10
piaip <piaip@csie.ntu.edu.tw>
2004-09-09 08:25:11 +0000

    reconf of awstats layout
    git-svn-id: http://svn.openfoundry.org/moztw@25 9cc691a1-34e3-0310-a474-e8860ed5959b

--- awstats/rebuild.sh
+++ awstats/rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 AWROOT=/home/moztw/awstats
-for cf in moztw.org forum.moztw.org
+for cf in sinica.moztw.org forum.moztw.org
 do
 	cd $AWROOT && make CONF=$cf update && make CONF=$cf output
 done
