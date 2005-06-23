0c3b06a02778c3edb9135e3fc8fd4feb9c1b44cf
piaip <piaip@csie.ntu.edu.tw>
2005-06-23 16:53:46 +0000

    misc update (nvu news)
    git-svn-id: http://svn.openfoundry.org/moztw@285 9cc691a1-34e3-0310-a474-e8860ed5959b

--- awstats/logrotate.sh
+++ awstats/logrotate.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+datetag=`perl -e 'my ($ss,$mm,$hh,$d,$m) = localtime(time);  $m++;
+	if($hh < 12) {($ss,$mm,$hh,$d,$m) = localtime(time-86400); $m++;}
+	 printf ("%02d%02d", $m,$d);'`
+
+if [ $datetag = '' ] ; then
+    datetag=`date +'%m%d'`
+fi
+
+echo "Logrotate: rotated to [$datetag]";
+
 LOGROOT=/home/moztw/log/raw/www
 # this script must be executed by root permission
 /usr/local/etc/rc.d/lighttpd.sh stop 2>&1 >/dev/null
 
-mv -f $LOGROOT/access.log $LOGROOT/access.`date +'%m%d'`.log 2>&1 >/dev/null
-mv -f $LOGROOT/error.log $LOGROOT/error.`date +'%m%d'`.log 2>&1 >/dev/null
+mv -f $LOGROOT/access.log $LOGROOT/access.$datetag.log 2>&1 >/dev/null
+mv -f $LOGROOT/error.log $LOGROOT/error.$datetag.log 2>&1 >/dev/null
 #touch $LOGROOT/access.log
 #touch $LOGROOT/error.log
 
