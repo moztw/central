b8f029710bb078090d835ebcf98edfda93015fa0
piaip <piaip@csie.ntu.edu.tw>
2005-07-04 03:13:18 +0000

    update to compress logs
    git-svn-id: http://svn.openfoundry.org/moztw@322 9cc691a1-34e3-0310-a474-e8860ed5959b

--- awstats/logrotate.sh
+++ awstats/logrotate.sh
@@ -3,6 +3,8 @@
 datetag=`perl -e 'my ($ss,$mm,$hh,$d,$m) = localtime(time);  $m++;
 	if($hh < 12) {($ss,$mm,$hh,$d,$m) = localtime(time-86400); $m++;}
 	 printf ("%02d%02d", $m,$d);'`
+compress_datetag=`perl -e 'my ($ss,$mm,$hh,$d,$m) = localtime(time-86400*3);  $m++;
+	 printf ("%02d%02d", $m,$d);'`
 
 if [ $datetag = '' ] ; then
     datetag=`date +'%m%d'`
@@ -15,9 +17,11 @@ LOGROOT=/home/moztw/log/raw/www
 /usr/local/etc/rc.d/lighttpd.sh stop 2>&1 >/dev/null
 
 mv -f $LOGROOT/access.log $LOGROOT/access.$datetag.log 2>&1 >/dev/null
-mv -f $LOGROOT/error.log $LOGROOT/error.$datetag.log 2>&1 >/dev/null
+
+#mv -f $LOGROOT/error.log $LOGROOT/error.$datetag.log 2>&1 >/dev/null
 #touch $LOGROOT/access.log
 #touch $LOGROOT/error.log
 
 /usr/local/etc/rc.d/lighttpd.sh start 2>&1 >/dev/null
-
+echo "Logrotate: compress [$compress_datetag]";
+nice -n 20 gzip $LOGROOT/access.$compress_datetag.log 2>&1 >/dev/null &
