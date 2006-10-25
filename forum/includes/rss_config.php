e912ab70da48bdaea17655d4d0282a46ed54f028
josesun <josesun@mail.moztw.org>
2006-10-25 15:05:03 +0000

    RSS config updated.
    git-svn-id: http://svn.openfoundry.org/moztw@557 9cc691a1-34e3-0310-a474-e8860ed5959b

--- forum/includes/rss_config.php
+++ forum/includes/rss_config.php
@@ -11,14 +11,14 @@ define('CACHE_TIME', 300);	    	// Cache time for RSS feed in sec, 5 min by defa
 									// Set to zero to disable cache (Not recomendated).
 define('AUTOSTYLED',false);			// Use XSLT transformation in MSIE by default
 define('CACHE_TO_FILE', false);		// Use cache dir for caching defaul page. You MUST set 777 to that dir first
-define('UPDATE_VIEW_COUNT',false);	// Update count of viewed topics for non-Anonymous user
+define('UPDATE_VIEW_COUNT',true);	// Update count of viewed topics for non-Anonymous user
 									// If set to false disable Who viewed a topic update too
-define('AUTO_WVT_MOD', true);    	// Who viewed a topic [2.0.6/EM]
+define('AUTO_WVT_MOD', false);    	// Who viewed a topic [2.0.6/EM]
 define('LV_MOD_INSTALLED',false);	// Last visit [2.0.10/EM]  http://mods.db9.dk/
 define('DEFAULT_ITEMS',25);			// How many items parsed in feed by default
 define('MAX_ITEMS',100);			// Max number of parsed items
-define('AUTOLOGIN',true);			// Try to autologin using cookie if user not set 'login' or 'uid' key
-define('SEE_MODIFYED',true);		// Include in feed modified from last visit record in addition to new records
+define('AUTOLOGIN',false);			// Try to autologin using cookie if user not set 'login' or 'uid' key
+define('SEE_MODIFYED',false);		// Include in feed modified from last visit record in addition to new records
 define('MAX_WEEKS_AGO',2);			// Limit RSS feed by time. Most important in large forums (!)
 									// Set to zero if you don't want use this futures
 $show_time=true;					// Show total script time in RSS comments.
