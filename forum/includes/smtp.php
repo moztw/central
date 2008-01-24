76d50d8e909e6eef6337e7c0f24fdd4acb1b7632
bobchao <bobchao@mail.moztw.org>
2008-01-24 08:51:36 +0000

    SMTP modification by Timdream
    git-svn-id: http://svn.openfoundry.org/moztw@757 9cc691a1-34e3-0310-a474-e8860ed5959b

--- forum/includes/smtp.php
+++ forum/includes/smtp.php
@@ -106,10 +106,23 @@ function smtpmail($mail_to, $subject, $message, $headers = '')
 
 	// Ok we have error checked as much as we can to this point let's get on
 	// it already.
-	if( !$socket = @fsockopen($board_config['smtp_host'], 25, $errno, $errstr, 20) )
+
+	// timdream: GO GMAIL
+	if( !$socket = @fsockopen('smtp.gmail.com', 465, $errno, $errstr, 20) )
+	//if( !$socket = @fsockopen($board_config['smtp_host'], 25, $errno, $errstr, 20) )
 	{
 		message_die(GENERAL_ERROR, "Could not connect to smtp host : $errno : $errstr", "", __LINE__, __FILE__);
 	}
+
+	// timdream: turn on stream_set_blocking as suggested in here:
+	//           http://tw.php.net/stream_socket_enable_crypto#75442
+	stream_set_blocking ($socket, true);
+	if( !stream_socket_enable_crypto($socket, true, STREAM_CRYPTO_METHOD_SSLv3_CLIENT))
+	{
+		message_die(GENERAL_ERROR, "Could not connect to smtp host securely.", "", __LINE__, __FILE__);
+	}
+	stream_set_blocking ($socket, false);
+	//timdream: SSL modification -->
 
 	// Wait for reply
 	server_parse($socket, "220", __LINE__);
@@ -120,7 +133,7 @@ function smtpmail($mail_to, $subject, $message, $headers = '')
 	{ 
 		fputs($socket, "EHLO " . $board_config['smtp_host'] . "\r\n");
 		server_parse($socket, "250", __LINE__);
-
+		
 		fputs($socket, "AUTH LOGIN\r\n");
 		server_parse($socket, "334", __LINE__);
 
