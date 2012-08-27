ab1b9865a835b6771346291e68a546d4e7cc4a49
petercpg <petercpg@mail.moztw.org>
2012-08-27 05:01:01 +0000

    translate: Update index.php to reflect Narro 2.0 upgrade
    git-svn-id: http://svn.openfoundry.org/moztw@1666 9cc691a1-34e3-0310-a474-e8860ed5959b

--- translate/index.php
+++ translate/index.php
@@ -8,7 +8,7 @@
 <body>
 	<h1>MozTW 翻譯站</h1>
 	<blockquote>
-		<p><strong>English: </strong>This is where MozTW hosts its "bridging" services to localizing Mozilla products. Primary, there is a <a href="http://code.google.com/">Narro</a> <a href="./narro/">installation</a> which separates files by products (with a project marked "shared" contains code for all Mozilla app). Some other scripts was also written to import and correct translations between repositories and Narro database. <strong>Any changes will and should always inspected by human before commit to the repository.</strong></p>
+		<p><strong>English: </strong>This is where MozTW hosts its "bridging" services to localizing Mozilla products. Primary, there is a <a href="https://code.google.com/p/narro/">Narro</a> <a href="./narro/">installation</a> which separates files by products (with a project marked "shared" contains code for all Mozilla app). Some other scripts was also written to import and correct translations between repositories and Narro database. <strong>Any changes will and should always inspected by human before commit to the repository.</strong></p>
 	</blockquote>
 	<p>歡迎光臨翻譯站，此站（第三代）僅供在地化團隊內部使用。這裡的翻譯建議並沒有被監控，想要回報錯誤請到討論區的<a href="http://forum.moztw.org/viewforum.php?f=4">在地化板回報</a>。有意長期貢獻請或接手翻譯專案的話請<a href="http://moztw.org/contribute/">加入我們</a>，到 mailing list 報到！</p>
 	<p>請前往 <a href="./narro/">Narro</a></p>
@@ -50,16 +50,10 @@
 	<hr />
 	<h2>翻譯流程 (給 l10n owners)</h2>
 	<ol>
-		<li>到 Manage 頁面 Export 最後的翻譯結果</li>
-		<li>到 Manage 頁面 Import 英文版的更新（經由 <a href="./update-log.txt">每日自動從 mozilla 更新的 repo</a>）</li>
-		<li>將 Accesskey 全部改為與英文版相同 <button class="correct-accesskey">同步</button></li>
-		<li><strong>輸入翻譯，Validate 確定的翻譯</strong>
-			<ul>
-				<li>更新 Narro 頁面快取，確定最新的翻譯進度統計資料 <button class="remove-cache">更新</button></li>
-			</ul>
-		</li>
-		<li>將 Accesskey 全部改為與英文版相同 <button class="correct-accesskey">同步</button></li>
-		<li>到 Manage 頁面 Export 最後的翻譯結果</li>
+		<li>到 Export 頁面匯出最後的翻譯結果</li>
+		<li>到 Import 頁面匯入英文版更新的字串（經由 <a href="./update-log.txt">每日自動從 mozilla 更新的 repo</a>）</li>
+		<li><strong>輸入翻譯，Validate 確定的譯文</strong></li>
+		<li>到 Export 頁面匯出最後的翻譯結果</li>
 		<li>更新你的工作目錄：<code>hg pull -u; hg update -r default</code> 或是 <code>svn update</code></li>
 		<li>下載 tar.gz、ssh 進來主機、或是用 scp 複製 <code>/home/moztw/translate/var/narro/data/import/*/zh-TW</code>的檔案，蓋掉你的工作目錄的舊檔案
 			<ul>
@@ -86,57 +80,28 @@
 	</ol>
 	<p>Windows 上面的作法請自行摸索 ...</p>
 	<h2>開新 Project 流程 (給 Narro maintainer)</h2>
-	<p>需要做這件事情的場合是如果 Mozilla 把 codebase split 出新的版本 (像是 <code>mozilla-central</code> →<code>releases/mozilla-1.9.2</code>，同時也開了對應的 l10n repository (像是 <code>releases/l10n-mozilla-1.9.2/zh-TW</code>)，需要在 Narro 開新的 Project 來維護的情況。</p>
+	<p>需要做這件事情的場合是如果 Mozilla 把 codebase split 出新的版本 (像是 <code>mozilla-central</code> →<code>releases/mozilla-1.9.2</code>，同時也開了對應的 l10n repository (像是 <code>releases/l10n-mozilla-1.9.2/zh-TW</code>)，需要在 Narro 開新的 Project 來維護的情況。<br>
+	在進入 Rapid Release Cycle 後 codebase 僅分為 <code>mozilla-central</code>、<code>mozilla-aurora</code>、<code>mozilla-beta</code>，所以這個情況可能不大容易再遇到...。</p>
 	<ol>
-		<li><code>cd /home/moztw/translate/var/repo;\<br />
-sudo -u www-data hg clone http://hg.mozilla.org/releases/mozilla-1.9.2 <strong>mozilla-1.9.2</strong></code>
-		<ul>
-			<li>repo 目錄底下的目錄都會被 crontab 自動更新</li>
-		</ul>
+		<li><code>cd /home/moztw/translate/var/repo;\<br>
+		sudo -u www-data hg clone http://hg.mozilla.org/releases/mozilla-1.9.2 <strong>mozilla-1.9.2</strong></code>
+			<ul>
+				<li>repo 目錄底下的目錄都會被 crontab 自動更新</li>
+			</ul>
 		</li>
-		<li>Narro: <a href="http://translate.moztw.org/narro/narro_project_edit.php?l=zh-TW">Add Project</a></li>
+		<li>Narro: <a href="narro/project_edit.php?l=zh-TW&amp;p=0">Add Project</a></li>
 		<li><code>sudo -u www-data mkdir -p /home/moztw/translate/var/narro/data/import/*/en-US</code></li>
 		<li><code>cd /home/moztw/translate/var/narro/data/import/*/en-US</code></li>
 		<li>一個一個建 symbolic link，例如
-		<ul>
-			<li><code>sudo -u www-data ln -s ../../../../../repo/<strong>mozilla-1.9.2</strong>/<strong>browser</strong>/locales/en-US <strong>browser</strong></code></li>
-			<li><code>sudo -u www-data mkdir -p other-licenses/branding;\<br />
-sudo -u www-data ln -s <strong>../../</strong>../../../../../repo/<strong>mozilla-1.9.2</strong>/<strong>other-licenses/branding/firefox</strong>/locales/en-US <strong>other-licenses/branding/firefox</strong></code></li>
-		</ul>
+			<ul>
+				<li><code>sudo -u www-data ln -s ../../../../../repo/<strong>mozilla-1.9.2</strong>/<strong>browser</strong>/locales/en-US <strong>browser</strong></code></li>
+				<li><code>sudo -u www-data mkdir -p other-licenses/branding;\<br>
+				sudo -u www-data ln -s <strong>../../</strong>../../../../../repo/<strong>mozilla-1.9.2</strong>/<strong>other-licenses/branding/firefox</strong>/locales/en-US <strong>other-licenses/branding/firefox</strong></code></li>
+			</ul>
 		</li>
 		<li>複製半成品翻譯到<code>/home/moztw/translate/var/narro/data/import/*/zh-TW</code></li>
-		<li>到 Manage 頁面 Import 英文版與翻譯</li>
-		<li>更新 Narro 頁面快取，確定最新的翻譯進度統計資料 <button class="remove-cache">更新</button></li>
-		<li>將 Accesskey 全部改為與英文版相同 <button class="correct-accesskey">同步</button></li>
-		<li>回到翻譯流程 #4</li>
+		<li>到 Import 頁面匯入原文與譯文字串</li>
+		<li>回到翻譯流程 #3</li>
 	</ol>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script type="text/javascript">
-$(function () {
-	$('.correct-accesskey').bind(
-		'click',
-		function () {
-			$.get(
-				'./narro/correct_accesskey.php',
-				function (data) {
-					alert(data);
-				}
-			);
-		}
-	);
-	$('.remove-cache').bind(
-		'click',
-		function () {
-			$.get(
-				'./narro/remove_cache.php',
-				function (data) {
-					alert(data);
-				}
-			);
-		}
-	);
-
-});
-	</script>
 </body>
 </html>
