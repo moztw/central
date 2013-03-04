d84883de3f3fec141c320ecf5653b8f92eedc886
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2013-03-04 23:44:41 +0800

    Update update-git.sh: don't use cd

--- autoupdate/update-git.sh
+++ autoupdate/update-git.sh
@@ -7,6 +7,8 @@ echo "using git: $GIT"
 echo "=============================="
 
 #browserpairs
-cd /home/moztw/htdocs/www/foxmosa/game/pairs
-$GIT pull origin master
+#cd /home/moztw/htdocs/www/foxmosa/game/pairs
+#$GIT pull origin master
+
+$GIT --git-dir=/home/moztw/htdocs/www/foxmosa/game/pairs/.git pull origin master
 
