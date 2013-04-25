40abe860266b838719e18c3dec4ede3cdb4b1587
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2013-04-25 22:25:10 +0800

    Update irc logbot status dection page & scripts

--- irclog/logstatus.php
+++ irclog/logstatus.php
@@ -1,16 +1,23 @@
 <?php
-function checkBot($process) {
-	exec("ps ax|grep " . $process, $result);
-	$length = count($result);
-	if ($length >= 2) {
-		return TRUE;
+function checkBot($pidfile) {
+	$filename = '/var/run/' . $pidfile;
+	if (file_exists($filename)) {
+		// if exists
+		$pid = intval(file_get_contents($filename)); // get pid as a integer
+		if ($pid === 1) {
+			die("Program error.");	// some basic protection of intval's retun value
+		}
+		if (is_dir("/proc/$pid")) {	// check if it is a valid pid and alive
+			return $pid;
+		} else {	// pidfile exists, but process died (unclean shutdown?)
+			return FALSE;
+		}
 	} else {
-		/* FIXME: Get PID of Bot process */
-		//$output = intval(substr($result[$length-2], 9, 5));
-		//echo $output;
 		return FALSE;
 	}
 }
+
+$botpid = checkBot("LumberJack.pid");
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,27 +27,26 @@ function checkBot($process) {
 </head>
 <body>
 	<h1>MozTW IRC Bot Status</h1>
-<? /* php  if (checkBot("LumberJack.py")) { */ ?>
-	<!--<p>The bot is Running!</p>--> 
-<? /* php  } else { */ ?>
+<?php if ($botpid !== FALSE) { ?>
+	<p>The bot is Running with PID <?php echo $botpid; ?>!</p> 
+<?php } else { ?>
 	<p>Call back IRC bot: <button class="respawn-ircbot">Respawn</button></p>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<script type="text/javascript">
-	$(function () {
-		$('.respawn-ircbot').bind(
-		'click',
-			function () {
-			$.get('./callbot.php',
-				function (data) {
-					alert(data);
-					location.reload();
+		$(function () {
+			$('.respawn-ircbot').bind(
+			'click',
+				function () {
+				$.get('./callbot.php',
+					function (data) {
+						alert(data);
+						location.reload();
+					}
+					);
 				}
-			     );
-			}
-		);
-
-	});
+			);
+		});
 	</script>
-<? /* php   }  (/   ?>
+<?php } ?>
 </body>
 </html>
